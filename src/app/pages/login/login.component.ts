6db05bbbc93eb215f119a1cf0c7874a7201b0d59
akansha290 <akanshakumari290@gmail.com>
2023-03-20 14:48:31 +0530

    Login Component is added with authentication which is provided by Backend.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginData={
+    username:'',
+    password:'',
+  };
+
+  constructor(private snack:MatSnackBar,
+    private login:LoginService){}
+
+  formSubmit(){
+    console.log("login Button Clicked");
+
+    if(this.loginData.username.trim()=='' || this.loginData.username==null){
+      this.snack.open("Username is required !!",'',{
+        duration:3000
+      });
+      return;
+    }
+
+    if(this.loginData.password.trim()=='' || this.loginData.password==null){
+      this.snack.open("Password is required !!",'',{
+        duration:3000
+      });
+      return;
+    }
+
+    //Request to Server to Generate Token
+    this.login.generateToken(this.loginData).subscribe(
+      (data:any)=>{
+        console.log('success');
+        console.log(data);
+      },
+      (error)=>{
+        console.log("Error !");
+        console.log(error);
+      }
+    );
+  }
 
 }
